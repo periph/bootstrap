559f6bd0825a845969fe12b0158e427a6a54b5df
Marc-Antoine Ruel <maruel@chromium.org>
2017-12-28 16:48:16 -0500

    img: Implement listSDCardsWindows() on Windows
    That's a start.

--- img/img_windows.go
+++ img/img_windows.go
@@ -6,6 +6,7 @@ package img
 
 import (
 	"errors"
+	"strings"
 )
 
 func flashWindows(imgPath, disk string) error {
@@ -21,5 +22,55 @@ func umountWindows(disk string) error {
 }
 
 func listSDCardsWindows() []string {
-	return nil
+	var out []string
+	for _, disk := range wmicListDisks() {
+		if disk["MediaLoaded"] == "TRUE" && disk["MediaType"] == "Removable Media" {
+			out = append(out, disk["DeviceID"])
+		}
+	}
+	return out
+}
+
+func wmicListDisks() []map[string]string {
+	// TODO(maruel): It'd be nicer to use the Win32 APIs but shelling out will be
+	// good enough for now. Shelling out permits to not have to do COM/WMI.
+	// In theory, /format:xml would produce an output meant to be mechanically
+	// parsed, in practice the output is suboptimal.
+	b, err := capture("", "wmic", "diskdrive", "list")
+	if err != nil || len(b) == 0 {
+		return nil
+	}
+	s := strings.Replace(string(b), "\r", "", -1)
+	lines := strings.Split(strings.TrimRight(s, "\n"), "\n")
+	type col struct {
+		start, end int
+		name       string
+	}
+	var cols []col
+	l := lines[0]
+	for i := 0; i < len(l); {
+		end := strings.IndexByte(l[i:], ' ')
+		if end == -1 {
+			// Last column.
+			end = len(l)
+		} else {
+			for end += i; end < len(l) && l[end] == ' '; end++ {
+			}
+		}
+		name := strings.TrimSpace(l[i:end])
+		if name == "" {
+			break
+		}
+		cols = append(cols, col{i, end, name})
+		i = end
+	}
+	var out []map[string]string
+	for i := 1; i < len(lines); i++ {
+		m := map[string]string{}
+		for _, c := range cols {
+			m[c.name] = strings.TrimSpace(lines[i][c.start:c.end])
+		}
+		out = append(out, m)
+	}
+	return out
 }
