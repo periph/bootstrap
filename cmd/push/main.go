periph/bootstrap

commit 0930af56ee24a737f0916d131e630598d821992c

img: fix processing of lsblk 2.34 output
They changed the json schema...

img/img.go
@@ -24,6 +24,7 @@ import (
 	"regexp"
 	"runtime"
 	"sort"
+	"strconv"
 	"strings"
 	"time"
 
@@ -319,15 +320,57 @@ var (
 	reMountLinux2 = regexp.MustCompile(`is already mounted at ` + "`" + `([^\']+)\'`)
 )
 
+// boolOrInt abstract the fact that on lsblk 2.31 some values are printed as
+// "0" or "1", but on 2.34 they are true or false.
+type boolOrInt bool
+
+func (b *boolOrInt) UnmarshalJSON(d []byte) error {
+	vb := false
+	if json.Unmarshal(d, &vb) == nil {
+		*b = boolOrInt(vb)
+		return nil
+	}
+	vi := 0
+	if err := json.Unmarshal(d, &vi); err != nil {
+		return err
+	}
+	*b = vi != 0
+	return nil
+}
+
+type intOrString int64
+
+func (i *intOrString) UnmarshalJSON(d []byte) error {
+	vi := int64(0)
+	if json.Unmarshal(d, &vi) == nil {
+		*i = intOrString(vi)
+		return nil
+	}
+	vs := ""
+	if err := json.Unmarshal(d, &vs); err != nil {
+		return err
+	}
+	v, err := strconv.ParseInt(vs, 10, 64)
+	if err != nil {
+		return err
+	}
+	*i = intOrString(v)
+	return nil
+}
+
+// blockDevice is the schema of the output of lsblk --json --bytes.
+//
+// The output changed somewhere between 2.31 and 2.34 so boolOrInt and
+// intOrString are used to hides the differences in the schema.
 type blockDevice struct {
 	Name string
 	// MajMin is the major and minor "device number".
 	MajMin string `json:"maj:min"`
 	// RM means removable media.
-	RM   string
-	Size int64 `json:",string"`
+	RM   boolOrInt
+	Size intOrString
 	// RO means read-only.
-	RO         string
+	RO         boolOrInt
 	Type       string
 	MountPoint string
 	Children   []blockDevice
@@ -341,7 +384,7 @@ type blockDevice struct {
 func (b *blockDevice) isSDCard() bool {
 	// Do not check for RM == "1". The reason is that for some embedded SD card
 	// readers (like Lenovo x250 embedded SD card reader), RM is set to "0". :(
-	if b.RO != "0" || b.Type != "disk" {
+	if b.RO || b.Type != "disk" {
 		return false
 	}
 	// Normally a disk is not mounted itself, as it contains partitions that
@@ -384,6 +427,7 @@ func listSDCardsLinux() []string {
 	}
 	v := lsblkOutput{}
 	if err = json.Unmarshal([]byte(b), &v); err != nil {
+		log.Printf("failed to parse lsblk output: %v", err)
 		return nil
 	}
 	var out []string

commit 4577df3707af6cde4b075aa2d61001f0f85ecf96

Fixes for checks that will be added soon and a regression
- Fix a regression from 0930af56ee24a737f0916d131e630598d821992c;
  boolOrInt was meant to be boolOrString.
- addlicense requires license header on all files.
- tool.String() didn't have the same receiver, triggering golint.
- img_windows had variable aliasing which triggered go vet shadow.
- Update go.mod

.gohci.yml
@@ -1,3 +1,7 @@
+# Copyright 2020 The Periph Authors. All rights reserved.
+# Use of this source code is governed under the Apache License, Version 2.0
+# that can be found in the LICENSE file.
+
 # See https://github.com/periph/gohci
 version: 1
 workers:

cmd/push/main.go
@@ -42,11 +42,11 @@ const toolName = "nonersyncrsyncpscpscp"
 
 var toolIndex = [...]uint8{0, 4, 9, 14, 18, 21}
 
-func (i tool) String() string {
-	if i < 0 || i >= tool(len(toolIndex)-1) {
-		return fmt.Sprintf("tool(%d)", i)
+func (t tool) String() string {
+	if t < 0 || t >= tool(len(toolIndex)-1) {
+		return fmt.Sprintf("tool(%d)", t)
 	}
-	return toolName[toolIndex[i]:toolIndex[i+1]]
+	return toolName[toolIndex[t]:toolIndex[t+1]]
 }
 
 func (t tool) push(verbose bool, src string, pkgs []string, host, rel string) error {

go.mod
@@ -5,7 +5,7 @@ go 1.11
 require (
 	github.com/rekby/mbr v0.0.0-20190325193910-2b19b9cdeebc
 	github.com/ulikunitz/xz v0.5.6
-	golang.org/x/crypto v0.0.0-20190701094942-4def268fd1a4
+	golang.org/x/crypto v0.0.0-20191011191535-87dc89f01550
 	golang.org/x/sys v0.0.0-20190626221950-04f50cda93cb
 	howett.net/plist v0.0.0-20181124034731-591f970eefbb
 )

go.sum
@@ -7,8 +7,8 @@ github.com/rekby/mbr v0.0.0-20190325193910-2b19b9cdeebc/go.mod h1:omSwqul59wlKxf
 github.com/ulikunitz/xz v0.5.6 h1:jGHAfXawEGZQ3blwU5wnWKQJvAraT7Ftq9EXjnXYgt8=
 github.com/ulikunitz/xz v0.5.6/go.mod h1:2bypXElzHzzJZwzH67Y6wb67pO62Rzfn7BSiF4ABRW8=
 golang.org/x/crypto v0.0.0-20190308221718-c2843e01d9a2/go.mod h1:djNgcEr1/C05ACkg1iLfiJU5Ep61QUkGW8qpdssI0+w=
-golang.org/x/crypto v0.0.0-20190701094942-4def268fd1a4 h1:HuIa8hRrWRSrqYzx1qI49NNxhdi2PrY7gxVSq1JjLDc=
-golang.org/x/crypto v0.0.0-20190701094942-4def268fd1a4/go.mod h1:yigFU9vqHzYiE8UmvKecakEJjdnWj3jj499lnFckfCI=
+golang.org/x/crypto v0.0.0-20191011191535-87dc89f01550 h1:ObdrDkeb4kJdCP557AjRjq69pTHfNouLtWZG7j9rPN8=
+golang.org/x/crypto v0.0.0-20191011191535-87dc89f01550/go.mod h1:yigFU9vqHzYiE8UmvKecakEJjdnWj3jj499lnFckfCI=
 golang.org/x/net v0.0.0-20190404232315-eb5bcb51f2a3/go.mod h1:t9HGtf8HONx5eT2rtn7q6eTqICYqUVnKs3thJo3Qplg=
 golang.org/x/sys v0.0.0-20190215142949-d0b11bdaac8a/go.mod h1:STP8DvDyc/dI5b8T5hshtkjS+E42TnysNCUPdjciGhY=
 golang.org/x/sys v0.0.0-20190412213103-97732733099d/go.mod h1:h1NjWce9XRLGQEsW7wpKNCjG9DtNlClVuFLEZdDNbEs=

img/img.go
@@ -320,21 +320,21 @@ var (
 	reMountLinux2 = regexp.MustCompile(`is already mounted at ` + "`" + `([^\']+)\'`)
 )
 
-// boolOrInt abstract the fact that on lsblk 2.31 some values are printed as
+// boolOrString abstract the fact that on lsblk 2.31 some values are printed as
 // "0" or "1", but on 2.34 they are true or false.
-type boolOrInt bool
+type boolOrString bool
 
-func (b *boolOrInt) UnmarshalJSON(d []byte) error {
+func (b *boolOrString) UnmarshalJSON(d []byte) error {
 	vb := false
 	if json.Unmarshal(d, &vb) == nil {
-		*b = boolOrInt(vb)
+		*b = boolOrString(vb)
 		return nil
 	}
-	vi := 0
+	vi := "0"
 	if err := json.Unmarshal(d, &vi); err != nil {
 		return err
 	}
-	*b = vi != 0
+	*b = vi != "0"
 	return nil
 }
 
@@ -360,17 +360,17 @@ func (i *intOrString) UnmarshalJSON(d []byte) error {
 
 // blockDevice is the schema of the output of lsblk --json --bytes.
 //
-// The output changed somewhere between 2.31 and 2.34 so boolOrInt and
+// The output changed somewhere between 2.31 and 2.34 so boolOrString and
 // intOrString are used to hides the differences in the schema.
 type blockDevice struct {
 	Name string
 	// MajMin is the major and minor "device number".
 	MajMin string `json:"maj:min"`
 	// RM means removable media.
-	RM   boolOrInt
+	RM   boolOrString
 	Size intOrString
 	// RO means read-only.
-	RO         boolOrInt
+	RO         boolOrString
 	Type       string
 	MountPoint string
 	Children   []blockDevice

img/img_windows.go
@@ -63,27 +63,25 @@ func flashWindows(imgPath, disk string) error {
 	var dummy uint32
 	var handles []syscall.Handle
 	for _, v := range getVolumesForDisk(disk, 0) {
-		r, err := syscall.UTF16PtrFromString(v)
-		if err != nil {
+		var r *uint16
+		if r, err = syscall.UTF16PtrFromString(v); err != nil {
 			return err
 		}
-		fd, err := syscall.CreateFile(r, syscall.GENERIC_READ|syscall.GENERIC_WRITE, 0, nil, syscall.OPEN_EXISTING, 0, 0)
-		if err != nil {
+		var fd syscall.Handle
+		if fd, err = syscall.CreateFile(r, syscall.GENERIC_READ|syscall.GENERIC_WRITE, 0, nil, syscall.OPEN_EXISTING, 0, 0); err != nil {
 			return fmt.Errorf("failed to open %s: %v", v, err)
 		}
 		// https://msdn.microsoft.com/en-us/library/windows/desktop/aa364575.aspx
 		// "Note that without a successful lock operation, a dismounted volume may
 		// be remounted by any process at any time"
-		err = syscall.DeviceIoControl(fd, fsctlLockVolume, nil, 0, nil, 0, &dummy, nil)
-		if err != nil {
+		if err = syscall.DeviceIoControl(fd, fsctlLockVolume, nil, 0, nil, 0, &dummy, nil); err != nil {
 			syscall.CloseHandle(fd)
 			return fmt.Errorf("failed to lock %s: %v", v, err)
 		}
 		// https://msdn.microsoft.com/en-us/library/windows/desktop/aa364562.aspx
 		//   "It is important to lock the volume first, otherwise unpredictable
 		//   behavior may happen."
-		err = syscall.DeviceIoControl(fd, fsctlDismountVolume, nil, 0, nil, 0, &dummy, nil)
-		if err != nil {
+		if err = syscall.DeviceIoControl(fd, fsctlDismountVolume, nil, 0, nil, 0, &dummy, nil); err != nil {
 			syscall.CloseHandle(fd)
 			return fmt.Errorf("failed to unmount %s: %v", v, err)
 		}